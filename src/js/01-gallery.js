27237bee90688cf7423abaea513a069e0ff12f55
mrabushyn <99904851+mrabushyn@users.noreply.github.com>
2022-08-22 20:38:17 +0300

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,33 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const galleryListItem = document.querySelector('.gallery');
+const createList = createListItem(galleryItems);
 
+function createListItem(items) {
+  return items
+    .map(
+      ({ preview, original, description }) =>
+        `<a class="gallery__item" href="${original}">
+<img class="gallery__image" src="${preview}" alt="" title="${description}"/>
+</a>`
+    )
+    .join('');
+}
 
+galleryListItem.insertAdjacentHTML('beforeend', createList);
+galleryListItem.addEventListener('click', largePicture);
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+
+function largePicture(e) {
+  e.preventDefault();
+
+  if (!e.target.classList.contains('gallery__image')) return;
+
+  gallery.on('show.simplelightbox');
+}
